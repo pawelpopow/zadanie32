43a0a998503f3969435f7ce6ee5166de084d2f62
ppopow <pawel.popow@gmail.com>
2020-10-18 18:24:15 +0200

    nowe zadanie 32

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let num1 = 2;
+let num2 = 5;
+
+function multiply(num8, num6) {
+    let num7;
+    if(num8 > num6) {
+        num7=num8;
+    } else {
+        num7=num6;
+    }
+    return num7;
+}
+    console.log(multiply(num1, num2));
